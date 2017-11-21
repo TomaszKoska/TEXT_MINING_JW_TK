efe865f61ede883b7505e8d67f61da9c8e1128d5
JacekWydra <jacek.wydra1992@gmail.com>
2017-11-21 22:47:47 +0100

    x

--- TEXT_MINING_JW_TK/databaseHelpers/SqliteHelper.py
+++ TEXT_MINING_JW_TK/databaseHelpers/SqliteHelper.py
@@ -71,8 +71,11 @@ class SqliteHelper(AbstractDatabaseHelper):
     def saveDocument(self,document = Document(), tableName = ""):
         #print("I tutaj Jacku dochodzi do zapiania dokumentu w bazie danych (tzn zaimplementuję to jak wrócę)")
         #print("argument numer 1 to dokument typu Document (zmieniłem ArticleClass na Document) a to drugie to nazwa tabeli jako string")
+        characters = [("'" , '"'),('"' , "'")]
+        [str(document.text).replace(x, y) for x, y in characters]
         query = "INSERT INTO " + tableName + " (TITLE, CONTENT, DATE, SOURCE) \
-          VALUES ('"+str(document.title) +"','"+str(document.text).translate({"'" : r"''"})+"','"+ str(document.date)+"','"+str(document.source)+"')"
+          VALUES ('"+str(document.title) +"','"+[str(document.text).replace(x, y) for x, y in characters] \
+              +"','"+ str(document.date)+"','"+str(document.source)+"')"
         #print(query)
         self.connection.execute(query);
         self.connection.commit()
