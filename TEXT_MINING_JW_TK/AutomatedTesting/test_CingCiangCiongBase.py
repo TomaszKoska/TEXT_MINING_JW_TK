94390c6ec46545057cce715d3a6895c10cd3881d
Tomasz Kośka <tomasz.koska1@gmail.com>
2017-11-23 15:54:22 +0100

    dodałem Base do CingCiangCiong

---
+++ TEXT_MINING_JW_TK/AutomatedTesting/test_CingCiangCiongBase.py
@@ -0,0 +1,12 @@
+import unittest
+
+class Test_CingCiangCiongBase(unittest.TestCase):
+
+
+
+
+    def test_A(self):
+        self.fail("Not implemented")
+
+if __name__ == '__main__':
+    unittest.main()
